16186df0d0f6148e3a50d6731942b4c6c46fda59
Matthew <mfleberfinger@gmail.com>
2018-12-24 16:26:39 -0500

    Added rudimentary debug ouput for use with headset on.

---
+++ Assets/Scripts/DebugMessenger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Simple script for displaying messages that can be read by a person wearing
+/// a VR headset.
+/// </summary>
+public class DebugMessenger : MonoBehaviour
+{
+	[Tooltip("The Text object to use for debug messages.")]
+	[SerializeField]
+	private Text m_debugText;
+
+
+	private void Update()
+	{
+		if(Input.GetButtonDown("Fire2"))
+			ToggleDisplay();
+	}
+
+	/// <summary>
+	/// Set the entire debug text area to a given string.
+	/// </summary>
+	/// <param name="text">The text to display.</param>
+	public void SetDebugText(string text)
+	{
+		m_debugText.text = text;
+	}
+
+	/// <summary>
+	/// Add a string to the displayed text on a new line.
+	/// </summary>
+	/// <param name="text">The text to display.</param>
+	public void ConcatDebugText(string text)
+	{
+		m_debugText.text = "\n" + m_debugText.text + " " + text;
+	}
+
+	/// <summary>
+	/// Hide the debug text if it is visible. If it is hidden, make it visible.
+	/// </summary>
+	public void ToggleDisplay()
+	{
+		if(m_debugText.enabled)
+			m_debugText.enabled = false;
+		else
+			m_debugText.enabled = true;
+	}
+}
