580909001b36c833f9711a51fc01826e95e23c35
Matthew <mfleberfinger@gmail.com>
2018-12-25 19:58:24 -0500

    Refactored thumbstick slightly. Added thumbstick to XR Rig prefab.

--- Assets/Scripts/thumbstick.cs
+++ Assets/Scripts/thumbstick.cs
@@ -2,28 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Allows the player to move the XR rig with the thumbsticks on the controllers.
 public class thumbstick : MonoBehaviour
 {
     [Tooltip("Thumbstick to use for X movement")]
     [SerializeField]
-    private string thumbStickIDX=null;
+    private string thumbStickIDX = "null";
     [Tooltip("Thumbstick to use for Z movement")]
     [SerializeField]
-    private string thumbStickIDY=null;
+    private string thumbStickIDY = "null";
     [Tooltip("Left Hand to Normalize Movement Against")]
     [SerializeField]
-    GameObject leftHand=null;
+    GameObject leftHand = null;
+	[Tooltip("Multiplier/coefficient to control player movement speed.")]
+    [SerializeField]
+	private float translationScaleFactor = 0.04f;
+	[Tooltip("Multiplier/coefficient to control player movement speed.")]
+    [SerializeField]
+	private float rotationSnapAngle = 30f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        float fwdInput = -Input.GetAxis(thumbStickIDY)/25f;
-        float sideInput = -Input.GetAxis(thumbStickIDX)/25f;
+		// Translation
+        float fwdInput = -Input.GetAxis(thumbStickIDY) * translationScaleFactor;
+        float sideInput = -Input.GetAxis(thumbStickIDX) * translationScaleFactor;
         Vector3 thisMove = new Vector3(0, 0, 0);
         Vector3 fwd = leftHand.transform.forward;
         fwd.y = 0;
@@ -32,5 +34,7 @@ public class thumbstick : MonoBehaviour
         thisMove += fwdInput * fwd;
         thisMove += sideInput * right;
         transform.position += thisMove;
+
+		// Rotation
     }
 }
