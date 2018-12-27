ff6d0fc2cb0d8afee0f38e8018c6f22e2a1f32d7
Matthew <mfleberfinger@gmail.com>
2018-12-27 18:19:01 -0500

    Added beating stick.

--- Assets/Scripts/Babysitter/Child.cs
+++ Assets/Scripts/Babysitter/Child.cs
@@ -10,9 +10,14 @@ public class Child : MonoBehaviour
 	[SerializeField]
 	private float forceMagnitude = 10f;
 
+	[Tooltip("Time for which the child stays still after being hit, in seconds.")]
+	[SerializeField]
+	private float stunTime = 10f;
+
 	private static Queue<TiltSensor> m_tiltSensors;
 
 	private NavMeshAgent m_agent;
+	private float m_stunCountdown;
 
 	private void Start()
 	{
@@ -24,6 +29,7 @@ public class Child : MonoBehaviour
 		// Initialize the destination value of NavMesh to allow us to check it
 		// properly in the if statement in Update().
 		m_agent.destination = transform.position;
+		m_stunCountdown = 0f;
 	}
 
 	private void Update()
@@ -33,12 +39,17 @@ public class Child : MonoBehaviour
 		// available. Repeat until out of things to break.
 		if (Mathf.Approximately(transform.position.x, m_agent.destination.x) &&
 			Mathf.Approximately(transform.position.z, m_agent.destination.z) &&
-			m_tiltSensors.Count > 0)
+			m_tiltSensors.Count > 0 && m_stunCountdown <= 0)
 		{
+			m_agent.isStopped = false;
 			m_agent.destination = m_tiltSensors.Dequeue().transform.position;
 		}
 		else if (m_tiltSensors.Count == 0)
-			m_agent.enabled = false;
+			m_agent.isStopped = false;
+		else if (m_stunCountdown > 0)
+			m_stunCountdown -= Time.deltaTime;
+		else if (m_stunCountdown <= 0)
+			m_agent.isStopped = false;
 	}
 
 	/// <summary>
@@ -59,5 +70,16 @@ public class Child : MonoBehaviour
 		if (other.tag == "Breakable")
 			other.GetComponent<Rigidbody>().
 				AddForce(Random.insideUnitSphere * forceMagnitude, ForceMode.Impulse);
+	}
+
+	/// <summary>
+	/// Beats the child (with love).
+	/// When this is called the child will be temporarily stunned and can be
+	/// carried.
+	/// </summary>
+	public void Hit()
+	{
+		m_agent.isStopped = true;
+		m_stunCountdown = stunTime;
 	}
 }
---
+++ Assets/Scripts/Babysitter/Discipliner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discipliner : MonoBehaviour
+{
+	// This must be a trigger because two kinematic rigidbodies won't detect
+	// collisions with each other.
+	private void OnTriggerEnter(Collider col)
+	{
+		Child m_child = col.gameObject.GetComponent<Child>();
+
+		// If this collides with a child while held by the player, hit the child.
+		if (gameObject.layer == 9 && m_child != null)
+			m_child.Hit();
+	}
+}
