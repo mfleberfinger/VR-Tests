c4cd37d121d2a88a550344f49e056a7a975c1fd5
Nyarmith <powah.serge@gmail.com>
2018-12-28 13:34:18 -0500

    defined models and coffeeslave

---
+++ Assets/Scripts/Barista/CoffeeSlave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CoffeeSlave : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject coffeeLine;
+    [SerializeField]
+    private GameObject finishLine;
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    private static Queue<int> m_List;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Barista/CoffeeSlaveSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeSlaveSpawner : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject prefab1;
+    [SerializeField]
+    public GameObject prefab2;
+    [SerializeField]
+    public GameObject prefab3;
+    [SerializeField]
+    public GameObject spawnLoc;
+    [SerializeField]
+    public float spawnTimer = 15.0f;
+
+    private float m_coolDownTime = 22.0f;
+    private List<GameObject> m_prefabs = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_prefabs.Add(prefab1);
+        m_prefabs.Add(prefab2);
+        m_prefabs.Add(prefab3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_coolDownTime -= Time.deltaTime;
+        if (m_coolDownTime < 0)
+        {
+            Instantiate(
+                m_prefabs[Random.Range(0,m_prefabs.Count)],
+                spawnLoc.transform.position,
+                Quaternion.identity);
+            m_coolDownTime = spawnTimer;
+        }
+    }
+}
