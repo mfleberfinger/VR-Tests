7a9aa166324d828a86fe7afb8561c8cd6affc36c
Zachary Ranes <zachary.mt.ranes@gmail.com>
2018-12-28 23:08:15 -0500

    overworld more or less

---
+++ Assets/Scripts/ContractSign.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContractSign : MonoBehaviour
+{
+    public int sceneToLoad = 1;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+        Debug.Log(other.tag);
+
+        if (other.tag == "Pen") 
+        {
+            SceneManager.LoadScene(sceneToLoad);
+            
+        }
+    }
+}
